f7a740eed3ee2c3aa7cf31694e30172090a7e572
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-21 11:29:07 +0330

    base description added

--- tests/unit/MessageDisplay.spec.js
+++ tests/unit/MessageDisplay.spec.js
@@ -1,4 +1,13 @@
 import MessageDisplay from "@/components/MessageDisplay";
 import { mount } from "@vue/test-utils";
 
-//testing if the message
+//testing if the call was successfull display the message
+//testing if the call was failed display the error
+describe("MessageDisplay", () => {
+    it("if service call was successfull display the message", async () => {
+        const wrapper = mount(MessageDisplay);
+    });
+    it("if message call failed display the error", async () => {
+        const wrapper = mount(MessageDisplay);
+    });
+});
