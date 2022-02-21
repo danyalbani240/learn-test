2b31b66851573ec5de62166d426c1cdc4c95179f
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-21 09:24:44 +0330

    LoginForm Component created

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <form @submit.prevent="handleLogin">
+            <input type="text" v-model="name" />
+            <button type="submit">submit</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: "",
+        };
+    },
+    emits: ["submit"],
+    methods: {
+        handleLogin() {
+            this.$emit("submit", this.name);
+        },
+    },
+};
+</script>
+
+<style></style>
