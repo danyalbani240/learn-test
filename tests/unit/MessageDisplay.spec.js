cf2737e5342fe1576a1d9478de152f20c15f0774
DanyalBani <mr.d.bani.d@gmail.com>
2022-02-21 11:48:47 +0330

    mockREsolved Once and mockMessage ddeclared

--- tests/unit/MessageDisplay.spec.js
+++ tests/unit/MessageDisplay.spec.js
@@ -6,6 +6,8 @@ jest.mock("@/service/axios.js");
 //testing if the call was failed display the error
 describe("MessageDisplay", () => {
     it("if service call was successfull display the message", async () => {
+        const mockMessage = "hello from danyal";
+        getMessage.mockResolvedValueOnce({ text: mockMessage });
         const wrapper = mount(MessageDisplay);
     });
     it("if message call failed display the error", async () => {
